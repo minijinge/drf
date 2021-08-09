95f3a7b7888a5b0d3821b4516288ca5ff1d70d35
minijinge <minijinge@163.com>
2021-08-09 14:32:37 +0800

    media资源路径配置

--- drf/settings.py
+++ drf/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+# 图片等media资源存放路径
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- drf/urls.py
+++ drf/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+from drf.settings import MEDIA_ROOT
 
 urlpatterns = [
+    # apps
+    path('users/', include('users.urls')),
+
+    # media资源路由配置
+    re_path(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
+
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
 ]
