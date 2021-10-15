67955244e98506e77ca17790fcfb8241bb7c4523
minijinge <minijinge@163.com>
2021-10-15 16:22:03 +0800

    解决报错：AttributeError: 'PosixPath' object has no attribute 'rstrip'

--- drf/settings.py
+++ drf/settings.py
@@ -15,7 +15,8 @@ import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
